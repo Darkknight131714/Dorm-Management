d6c04392e8f9df67cce68e05ba5499e287efc1d9
Gautam <darkknight131714@gmail.com>
2022-01-23 05:56:46 +0530

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'loginpage.dart';
 import 'admin_homepage.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const Loginpage(),
+      home: const SplashScreen(),
       //theme: ThemeData.(),
       theme: ThemeData(
         brightness: Brightness.light,
@@ -23,6 +24,52 @@ class MyApp extends StatelessWidget {
         // accentColor: Color(0xFF1DA1F2),
         canvasColor: Color(0xFFE5E5E5),
       ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    super.initState();
+    Timer(
+      Duration(seconds: 1),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (BuildContext context) => Loginpage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg.png"), fit: BoxFit.fill),
+          ),
+        ),
+        Align(
+          child: CircularProgressIndicator(
+            color: Colors.white,
+          ),
+          alignment: Alignment.bottomCenter,
+        )
+      ]),
     );
   }
 }
